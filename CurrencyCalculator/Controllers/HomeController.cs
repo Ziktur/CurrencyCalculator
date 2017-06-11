f0423a4c1e3cb56b74a4c0c3a9bb3bae44e1f9be
Ziktur <ziktur@gmail.com>
2017-06-11 15:51:56 +0500

    Added files

---
+++ CurrencyCalculator/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CurrencyCalculator.Models;
+
+namespace CurrencyCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        float GetCourse(string currency)
+        {
+            WebRequest request = WebRequest.Create("http://www.cbr.ru/scripts/XML_daily.asp");
+            WebResponse response = request.GetResponse();
+            XDocument xDoc = XDocument.Load(response.GetResponseStream());
+            var currencyNode = (from node in xDoc.Element("ValCurs").Elements("Valute")
+                                where node.Element("CharCode").Value == currency
+                                select node).First();
+            int nominal = int.Parse(currencyNode.Element("Nominal").Value);
+            float value = float.Parse(currencyNode.Element("Value").Value);
+            return value / nominal;
+        }
+
+        public ActionResult Index()
+        {
+            ViewBag.currencyList = DataToConvert.SelectList;
+            return View();
+        }
+
+        public ActionResult RubToVal(DataToConvert dataInput)
+        {
+            try
+            {
+                ViewBag.result = (dataInput.SummInRub / GetCourse(dataInput.Currency)).ToString();
+                ViewBag.currencyList = DataToConvert.SelectList;
+                ViewBag.currency = dataInput.Currency;
+                return View("Result", dataInput);
+            }
+            catch
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult ValToRub(DataToConvert dataInput)
+        {
+            try
+            {
+                ViewBag.result = (dataInput.SummInVal * GetCourse(dataInput.Currency)).ToString();
+                ViewBag.currencyList = DataToConvert.SelectList;
+                ViewBag.currency = "RUB";
+                return View("Result", dataInput);
+            }
+            catch
+            {
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
---
+++ CurrencyCalculator/Models/DataToConvert.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace CurrencyCalculator.Models
+{
+    public class DataToConvert
+    {
+        public static List<string> CharCodes { get; } = new List<string> { "USD", "EUR", "GBP", "CNY", "JPY", "UAH" };
+        public static SelectList SelectList { get; } = CreateList();
+
+        [Display(Name = "Валюта")]
+        public string Currency { get; set; }
+
+        [Display(Name = "Сумма в рублях")]
+        public float SummInRub { get; set; }
+
+        [Display(Name = "Сумма в валюте")]
+        public float SummInVal { get; set; }
+
+        static SelectList CreateList()
+        {
+            var charCodeList = CharCodes.Select(x => new SelectListItem { Value = x, Text = x });
+            return new SelectList(charCodeList, "Value", "Text", "USD");
+        }
+    }
+}
